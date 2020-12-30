b96633849c3c90e180a627060fb73f442030e4fc
youufis <youufis@sina.com>
2020-12-30 10:15:48 +0800

    记录访问ip

--- sc/views.py
+++ sc/views.py
@@ -14,6 +14,28 @@ from django.conf import settings
 from sc.addnews import newsform
 from django.contrib import messages
 import random
+import socket
+
+def get_host_ip():
+    """
+    查询本机ip地址
+    :return:
+    """
+    try:
+        s=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+        s.connect(('8.8.8.8',80))
+        ip=s.getsockname()[0]
+    finally:
+        s.close()
+    return ip
+
+def get_client_ip(request):
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]  # 所以这里是真实的ip
+    else:
+        ip = request.META.get('REMOTE_ADDR')  # 这里获得代理ip
+    return ip
 
 
 def check_code(request):
@@ -87,6 +109,10 @@ def register(request):
 ##########################################    
          
 def index(request):
+
+    hostip=get_host_ip()
+    clientip=get_client_ip(request)
+        
     file_dir=os.path.join(settings.MEDIA_ROOT,"images")
     fname=random.sample(os.listdir(file_dir),4)
 
